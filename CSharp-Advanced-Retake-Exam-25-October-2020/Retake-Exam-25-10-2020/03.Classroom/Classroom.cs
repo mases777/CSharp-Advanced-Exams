ef288213f830734ad833ad2327587bd430fef284
mases777 <mases@abv.bg>
2022-08-01 18:48:21 +0300

    Exams03

---
+++ CSharp-Advanced-Retake-Exam-25-October-2020/Retake-Exam-25-10-2020/03.Classroom/Classroom.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.Classroom
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public int Count
+        {
+            get
+            {
+                return students.Count;
+            }
+            private set { }
+        }
+        public Classroom(int capacity)
+        {
+            students = new List<Student>();
+            this.Capacity = capacity;
+        }
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else return "No seats in the classroom";
+        }
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student removeStudent = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            if (removeStudent != null)
+            {
+                students.Remove(removeStudent);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else return "Student not found";
+        }
+        public string GetSubjectInfo(string subject)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<Student> studentSubject = students.Where(x => x.Subject == subject).ToList();
+            if (studentSubject.Count != 0)
+            {
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (var item in studentSubject)
+                {
+                    sb.AppendLine($"{item.FirstName} {item.LastName}");
+                }
+            }
+            else sb.AppendLine("No students enrolled for the subject");
+            return sb.ToString().TrimEnd();
+        }
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student getStudent = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            return getStudent;
+        }
+    }
+}
---
+++ CSharp-Advanced-Retake-Exam-25-October-2020/Retake-Exam-25-10-2020/03.Classroom/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Classroom
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string subject;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+        public Student(string firstName, string lastName, string subject)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Subject = subject;
+        }
+        public override string ToString()
+        {
+            return $"Student: First Name = {this.FirstName}, Last Name = {this.LastName}, Subject = {this.Subject}";
+        }
+    }
+}
