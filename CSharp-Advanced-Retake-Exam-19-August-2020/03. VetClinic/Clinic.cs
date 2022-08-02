02ec8b1628f4a1161334dbb88c2eb643e4859e65
mases777 <mases@abv.bg>
2022-08-02 16:48:17 +0300

    Exams03

---
+++ CSharp-Advanced-Retake-Exam-19-August-2020/03. VetClinic/Clinic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;        
+        private int capacity;        
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+            private set { }
+
+        }
+        public Clinic(int capacity)
+        {
+            data = new List<Pet>();            
+            this.Capacity = capacity;
+        }
+        public void Add(Pet pet)
+        {
+            if (data.Count + 1 <= Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(x => x.Name == name);
+            if (pet != null)
+            {
+                data.Remove(pet);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Pet GetOldestPet()
+        {
+            Pet pet = data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return pet;
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+            return pet;
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The clinic has the following patients:");
+
+            foreach (var item in data)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-Retake-Exam-19-August-2020/03. VetClinic/Pet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        private string name;
+        private int age;
+        private string owner;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
