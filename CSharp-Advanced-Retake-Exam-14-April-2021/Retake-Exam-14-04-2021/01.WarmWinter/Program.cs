12b0e6d18ae7a7ea173b8188df6ba88bb222ea21
mases777 <mases@abv.bg>
2022-08-05 17:22:31 +0300

    Exams01

---
+++ CSharp-Advanced-Retake-Exam-14-April-2021/Retake-Exam-14-04-2021/01.WarmWinter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputHats = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] inputScarfs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> hats = new Stack<int>(inputHats);
+            Queue<int> scarfs = new Queue<int>(inputScarfs);
+
+            List<int> clothes = new List<int>();
+
+            while (scarfs.Count > 0 && hats.Count > 0)
+            {
+                if (hats.Peek() == scarfs.Peek())
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hats.Pop() + 1);
+                }
+                else if (scarfs.Peek() > hats.Peek())
+                {
+                    hats.Pop();
+                }
+                else if (hats.Peek() > scarfs.Peek())
+                {
+                    clothes.Add(hats.Pop() + scarfs.Dequeue());
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {clothes.Max()}");
+            Console.WriteLine(string.Join(" ", clothes));
+        }
+    }
+}
