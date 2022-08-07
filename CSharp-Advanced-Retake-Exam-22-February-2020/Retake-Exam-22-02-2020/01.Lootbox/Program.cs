5fd578388866b4c42e95a6f9ea1470e51f17aa31
mases777 <mases@abv.bg>
2022-08-07 20:05:50 +0300

    Exams01

---
+++ CSharp-Advanced-Retake-Exam-22-February-2020/Retake-Exam-22-02-2020/01.Lootbox/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Lootbox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] queue = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] stack = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> first = new Queue<int>(queue);
+            Stack<int> second = new Stack<int>(stack);
+
+            List<int> claimed = new List<int>();
+
+            while (true)
+            {
+                if ((first.Peek() + second.Peek()) % 2 == 0)
+                {
+                    claimed.Add(first.Dequeue() + second.Pop());
+                }
+                else
+                {
+                    first.Enqueue(second.Pop());
+                }
+
+                if (first.Count == 0)
+                {
+                    Console.WriteLine("First lootbox is empty");
+                    break;
+                }
+
+                if (second.Count == 0)
+                {
+                    Console.WriteLine("Second lootbox is empty");
+                    break;
+                }
+            }
+
+            if (claimed.Sum() >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimed.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimed.Sum()}");
+            }
+        }
+    }
+}
