5168c12d78210d5e6a4eceacdbbb504f9be584a9
mases777 <mases@abv.bg>
2022-08-11 12:09:31 +0300

    Exams03

---
+++ CSharp-Advanced-Retake-Exam-18-August-2021/Retake-Exam-18-08-2021/03.StreetRacing/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private string licensePlate;
+        private int horsePower;
+        private double weight;
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string LicensePlate
+        {
+            get { return licensePlate; }
+            set { licensePlate = value; }
+        }
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"License Plate: {LicensePlate}");
+            sb.AppendLine($"Horse Power: {HorsePower}");
+            sb.AppendLine($"Weight: {Weight}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-Retake-Exam-18-August-2021/Retake-Exam-18-08-2021/03.StreetRacing/Race.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        private List<Car> data;
+        private string name;
+        private string type;
+        private int laps;
+        private int capacity;
+        private int maxHorsePower;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int Laps
+        {
+            get { return laps; }
+            set { laps = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public int MaxHorsePower
+        {
+            get { return maxHorsePower; }
+            set { maxHorsePower = value; }
+        }
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+            private set { }
+        }
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            data = new List<Car>();
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+        }
+
+        public void Add(Car car)
+        {
+            if (data.Count + 1 <= Capacity)
+            {
+                data.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            Car car = data.FirstOrDefault(x => x.LicensePlate == licensePlate);
+            if (car != null)
+            {
+                data.Remove(car);
+                return true;
+            }
+            return false;
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            Car car = data.FirstOrDefault(x => x.LicensePlate == licensePlate);
+            return car;
+        }
+        public Car GetMostPowerfulCar()
+        {
+            Car car = data.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+            return car;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
